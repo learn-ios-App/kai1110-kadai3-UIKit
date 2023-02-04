fc5451bf6c7e0e7f4373f4f0df9061618c27bd92
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-04 22:01:12 +0900

    commit

--- kai1110-kadai3-UIKit/ViewController.swift
+++ kai1110-kadai3-UIKit/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  kai1110-kadai3-UIKit
-//
-//  Created by 渡邊魁優 on 2023/02/04.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var numberText1: UITextField!
+    @IBOutlet private weak var numberText2: UITextField!
+    @IBOutlet private weak var swich1: UISwitch!
+    @IBOutlet private weak var swich2: UISwitch!
+    @IBOutlet private weak var numberLabel1: UILabel!
+    @IBOutlet private weak var numberLabel2: UILabel!
+    @IBOutlet private weak var result: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction private func calclation(_ sender: Any) {
+        var number1 = Int(numberText1.text ?? "") ?? 0
+        var number2 = Int(numberText2.text ?? "") ?? 0
+
+        if swich1.isOn {
+            numberLabel1.text =  "-" + String(number1)
+            number1 *= -1
+        }
+        else {
+            numberLabel1.text = String(number1)
+        }
+
+        if swich2.isOn {
+            numberLabel2.text = "-" + String(number2)
+            number2 *= -1
+        }
+        else {
+            numberLabel2.text = String(number2)
+        }
+
+        result.text = String(number1 + number2)
     }
-
-
 }
-
